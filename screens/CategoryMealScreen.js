3391deabac95f95e6d89785e01c20677799b0e2d
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-11-07 08:20:14 -0300

    Indo para tela de detalhes de refeições

--- screens/CategoryMealScreen.js
+++ screens/CategoryMealScreen.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const CategoryMealScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <Button
+        title="Ir para tela de Detalhe de Refeição!"
+        onPress={() =>
+          props.navigation.navigate({
+            routeName: "MealDetail"
+          })
+        }
+      />
     </View>
   );
 };

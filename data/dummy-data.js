8507f17f670ca9aea0083353fc40964cf9bc1c99
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-11-07 09:39:10 -0300

    Mostrando o grid de categorias

---
+++ data/dummy-data.js
@@ -0,0 +1,14 @@
+import Category from "../models/category";
+
+export const CATEGORIES = [
+  new Category("c1", "Italiana", "#f5428d"),
+  new Category("c2", "Rápida e Fácil", "#f54242"),
+  new Category("c3", "Hamburguers", "#f5a442"),
+  new Category("c4", "Alemã", "#f5d142"),
+  new Category("c5", "Leves", "#368dff"),
+  new Category("c6", "Exótica", "#41d95d"),
+  new Category("c7", "Café-da-manhã", "#9eecff"),
+  new Category("c8", "Asiática", "#b9ffb0"),
+  new Category("c9", "Francesa", "#ffc7ff"),
+  new Category("c10", "Verão", "#47fced")
+];
--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -1,19 +1,23 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, FlatList, Text } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = itemData => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoryScreen = props => {
   return (
-    <View style={styles.screen}>
-      <Text>The CategoryScreen!</Text>
-      <Button
-        title="Ir para tela de Refeições!"
-        onPress={() =>
-          props.navigation.navigate({
-            routeName: "CategoryMeals"
-          })
-        }
-      />
-    </View>
+    <FlatList
+      numColumns={2}
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+    />
   );
 };
 
@@ -22,6 +26,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
 

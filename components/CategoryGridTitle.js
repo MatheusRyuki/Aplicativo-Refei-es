e5d181b0a802cb0096b2f086c142bc45b54eac4e
Matheus ryuki Medeiros kaiya <dr_4_gao@hotmail.com>
2019-11-08 09:43:36 -0300

    Consertando as sombras

--- components/CategoryGridTitle.js
+++ components/CategoryGridTitle.js
@@ -26,6 +26,7 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
+    elevation: 5,
     overflow: "hidden"
   },
   container: {
@@ -35,7 +36,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: "flex-end",
     alignItems: "flex-end"
